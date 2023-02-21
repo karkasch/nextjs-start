dd7638296c599a76508370c8201dd3ebc5bbe7d5
karkasch <nutzer12@gmail.com>
2023-02-21 11:00:35 +0100

    return json

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -1,15 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getUsers, UserDocument } from '../../../lib/db/users';
 
-type Data = {
-  id: string;
-  name: string | null;
-  isAuthorized: boolean;
-}
+// type Data = {
+//   id: string;
+//   name: string | null;
+//   isAuthorized: boolean;
+// }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<UserDocument[]>
 ) {
-  res.status(200).json({ id: '0', name: null, isAuthorized: false });
+  // res.status(200).json({ id: '0', name: null, isAuthorized: false });
+
+  const users = await getUsers();
+  res.status(200).json(users);
 }
